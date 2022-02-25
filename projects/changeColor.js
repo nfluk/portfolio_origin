0b06e649bd9698d9779a69967e63761638d6bdc4
deroj <nickylukic@gmail.com>
2022-02-25 15:50:52 +0100

    first commit

---
+++ projects/changeColor.js
@@ -0,0 +1,21 @@
+let colorOne = "antiquewhite";
+let colorTwo = "aqua";
+let colorThree = "aquamarine";
+let colorFour = "crimson";
+let colorFive = "lightgreen";
+let colorSix = "deeppink";
+
+var colorArray = [
+  colorOne,
+  colorTwo,
+  colorThree,
+  colorFour,
+  colorFive,
+  colorSix,
+];
+
+function changeColor() {
+  var randNumber = Math.floor(Math.random() * colorArray.length);
+
+  document.getElementById("box").style.backgroundColor = colorArray[randNumber];
+}
