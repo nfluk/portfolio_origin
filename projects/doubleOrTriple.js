6af8f265694cd4ad295da8b18938096998c99ca3
deroj <nickylukic@gmail.com>
2022-02-26 16:34:54 +0100

    first commit

---
+++ projects/doubleOrTriple.js
@@ -0,0 +1,15 @@
+function double() {
+  var number = document.getElementById("number").value;
+
+  var result = number * 2;
+
+  document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+function triple() {
+  var number = document.getElementById("number").value;
+
+  var result = number * 3;
+
+  document.getElementById("result").innerHTML = "Result: " + result;
+}
