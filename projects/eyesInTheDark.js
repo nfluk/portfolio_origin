1f7bb427b577644cc5335ba3fca2ac4a5fa919a3
deroj <nickylukic@gmail.com>
2022-03-03 16:09:15 +0100

    in pixles getting position of mouse

--- projects/eyesInTheDark.js
+++ projects/eyesInTheDark.js
@@ -2,3 +2,7 @@ var iris = document.querySelectorAll(".iris");
 
 window.addEventListener("mousemove", (e) => {
     e = e || window.event;
+
+//in pixels the position of the mouse
+var { pageX, pageY } = e;
+var { innerWidth, innerHeight } = window;
